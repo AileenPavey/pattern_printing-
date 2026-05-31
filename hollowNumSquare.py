fff06f9f38b637393024defe77458ea6b2f71aa2
AileenPavey <pavey.aillen@gmail.com>
2026-05-31 17:15:27 +0530

    added hollow numbers square

---
+++ hollowNumSquare.py
@@ -0,0 +1,10 @@
+n=4
+for i in range (1,n+1):
+	for j in range (1,n+1):
+		if (i==1 or i==n):
+			print ( j ,end=" ")
+		elif(j==1 or j==n):
+			print( i ,end=" ")
+		else:
+			print(" ",end=" ")
+	print()
